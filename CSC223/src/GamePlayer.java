b69aeae8c066d8a2f96a01469e4b047203235796
John Lizama <147176920+john3lizama@users.noreply.github.com>
2024-11-15 10:32:17 -0500

    Things to figure out: 1. Aligning visited with actual map 2. Properly implementing DFS 3. Backtracking

--- CSC223/src/GamePlayer.java
+++ CSC223/src/GamePlayer.java
@@ -1,6 +1,7 @@
 import game.Game;												 //import the required library for game
 import game.exception.GamePlayException;
 import game.exception.InvalidGameActionException;
+import game.exception.InvalidGameMoveException;
 import java.util.Stack;   										 //using stacks for DFS strategy to game player
 import java.util.LinkedList;									 //using linked list for internal memory
 
@@ -11,6 +12,15 @@ public class GamePlayer {
 			this.x = x;
 			this.y = y;
 		}
+		public boolean equals(Object obj) {
+			if (this == obj) return true;  						//same reference
+			if (obj == null || getClass() != obj.getClass()) return false;  // Check type
+			Position other = (Position) obj;
+			return this.x == other.x && this.y == other.y;  	//check if x and y are equal
+		}
+		public String toString() {								//giving the output more readability
+			return "(" + x + ", " + y + ")";
+		}
 	}
 	
 	static Stack<Position> stack = new Stack<>();
@@ -18,30 +28,29 @@ public class GamePlayer {
 	
 	private static int x, y;  									//x= rows, y= columns
 	
-
 	private static void moveDown() {                            //AI will move down if it's allowed
 		stack.push(new Position(x, y));
-		System.out.println("Moving down to (" + ++x + ", " + y + ")");
 		visited.add(new Position(x,y));
+		++x;
 	}
 	private static void moveUp() { 								//AI will move down if it's allowed
-		stack.pop();
-		System.out.println("Moving up to (" + --x + ", " + y + ")");
-		//visited.add(new Position(x,y));
+		stack.push(new Position(x, y));
+		visited.add(new Position(x,y));
+		--x;
 	}
 	private static void moveRight() { 							//AI will move down if it's allowed
 		stack.push(new Position(x, y));
-		System.out.println("Moving right to (" + x + ", " + ++y + ")");
 		visited.add(new Position(x,y));
+		++y;	
 	}
 	private static void moveLeft() { 							//AI will move down if it's allowed
-		stack.pop();
-		System.out.println("Moving left to (" + x + ", " + --y + ")");
-		//visited.add(new Position(x,y));
+		stack.push(new Position(x, y));
+		visited.add(new Position(x,y));
+		--y;
 	}
 	
 	private static boolean isVisited(int x, int y){				//boolean to check if it is visited
-		return visited.contains(new Position(x, y));
+		return visited.contains(new Position(x, y));			//uses an equals method to compare
 	}
 	
 	private static boolean canMoveDown() { 						//AI will check if it is able to move down
@@ -58,60 +67,57 @@ public class GamePlayer {
 	}
 	
 	
-	private static void explore() throws GamePlayException, InvalidGameActionException {//Main AI function of how it is getting around
+	private static void explore() throws GamePlayException, InvalidGameActionException, InvalidGameMoveException {//Main AI function of how it is getting around
 		if (Game.hasShovel()) {
-			Game.pickShovel();
 			System.out.println("Found the shovel at (" + x + ", " + y + ")");
-			
+			Game.pickShovel();
 		}
 		if (Game.hasTreasure()) {
-			System.out.println("Found the treasure at (" + x + ", " + y + ")");
-			Game.digTreasure();
+			System.out.println("Found the treasure at (" + x + ", " + y + ")");    //Found at (6, 2)
+			if (Game.hasShovel()) {
+				Game.digTreasure();
+			}
 		}
-		if (canMoveDown()) {
-			 moveDown();
+		try {
+			if (canMoveDown()) {
+				moveDown();
+				Game.moveDown();
+			}
+			else if (canMoveRight()) {
+				moveRight();
+				Game.moveRight();
+			}
+			else if (canMoveUp()) {
+				moveUp();
+				Game.moveUp();
+			}
+			else if (canMoveLeft()) {
+				moveLeft();
+				Game.moveLeft();
+			}
+		} catch (Exception e) {
+			System.out.println("Can't move there, finding a away around.");
 		}
-		else if (canMoveRight()) {
-			moveRight();	
-		}
-		else if (canMoveUp()) {
-			moveUp();
-		}
-		else if (canMoveLeft()) {
-			moveLeft();
-		}
-		 /*if (!Game.hasShovel() && !Game.hasTreasure()) {
-			 explore();
-		 } else return;*/
-	}
+		if (!Game.hasShovel() && stack.size() < 48) {
+		 explore();
+		 } else {
+			 return;
+		 }
+	} 
 		
-	public static void main(String[] args) throws GamePlayException, InvalidGameActionException {
-		Game.play();
+	public static void main(String[] args) throws GamePlayException, InvalidGameActionException, InvalidGameMoveException {
+		Game.play();	
 		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		explore();
-		
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
 		
 		if (!Game.hasTreasure()) {
 			System.out.println();
-			System.out.println("Could not complete task.");
+			System.out.println("Could not complete task.");	
 		}
+		
+		Game.quit();
 	} 
 }
 
